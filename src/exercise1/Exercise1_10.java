3ac4eefb369bedc124f462e66e15dc6aa12e6a9c
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 10:10:00 +0900

    Exercise1_10@練習問題1-10

---
+++ src/exercise1/Exercise1_10.java
@@ -0,0 +1,11 @@
+package exercise1;
+
+public class Exercise1_10 {
+	public static void main(String[] args) {
+		int x = 10;
+		int x1 = ((int)Math.pow(x, 1));
+		int x2 = ((int)Math.pow(x, 2));
+		int x3 = ((int)Math.pow(x, 3));
+		System.out.println("xの1乗:" + x1 + ",xの2乗:" + x2 + ",xの3乗:" + x3);
+	}
+}

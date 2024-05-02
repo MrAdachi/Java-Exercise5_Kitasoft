4a1b24b74f89a05e0cd51836958ef2e6da4c9e5f
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 22:58:16 +0900

    Exercise4_3@練習問題4-3

---
+++ src/exercise4/Chapter4_3.java
@@ -0,0 +1,19 @@
+package exercise4;
+
+public class Chapter4_3 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 8; i++) {
+			System.out.println((int)Math.pow(2, i));
+		}
+		
+		System.out.println("----------------------------");
+		
+		int n = 1;
+		
+		for(int j = 1; j <= 8; j++) {
+			int ans = n * 2;
+			System.out.println(ans);
+			n = ans;
+		}
+	}
+}

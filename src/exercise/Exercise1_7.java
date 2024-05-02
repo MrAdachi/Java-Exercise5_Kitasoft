9dd9c159b0fdd729d1897228994356607b326887
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 09:44:36 +0900

    Exercise1_7@練習問題1-7

---
+++ src/exercise/Exercise1_7.java
@@ -0,0 +1,13 @@
+package exercise;
+
+public class Exercise1_7 {
+	public static void main(String[] args) {
+		int x = 10;
+		int y = 100;
+		
+		int z = x; //xの値を格納(z)
+		x = y;
+		y = z;
+		System.out.println("x=" + x + ",y=" + y);
+	}
+}

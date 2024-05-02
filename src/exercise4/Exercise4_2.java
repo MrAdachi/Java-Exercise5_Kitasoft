05976dd5a9c8364b172d4103f79444c1dec8a7be
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 22:42:27 +0900

    Exercise4_2@練習問題4-2

---
+++ src/exercise4/Exercise4_2.java
@@ -0,0 +1,15 @@
+package exercise4;
+
+public class Exercise4_2 {
+	public static void main(String[] args) {
+		for(int i = 1; i <= 9; i++) {
+			System.out.println(i * 3);
+		}
+		
+		System.out.println("---------------------------");
+		
+		for(int j = 3; j <= 27; j += 3) {
+			System.out.println(j);
+		}
+	}
+}

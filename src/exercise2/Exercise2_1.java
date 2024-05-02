6534fdae35c6d5166e2986399396f50e14c6ddc9
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 11:42:08 +0900

    Exercise2_1@練習問題2-1

---
+++ src/exercise2/Exercise2_1.java
@@ -0,0 +1,14 @@
+package exercise2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exercise2_1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader( System.in ) );
+		
+		String s = br.readLine(); // 1行分の文字列を入力
+		System.out.println("入力文字は、" + s + "です。");
+	}
+}

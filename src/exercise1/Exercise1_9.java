b52d4d6b747015b650c10b07c5eddcf022aa77a8
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 10:02:23 +0900

    Exercise1_9@練習問題1-9

---
+++ src/exercise1/Exercise1_9.java
@@ -0,0 +1,8 @@
+package exercise1;
+
+public class Exercise1_9 {
+	public static void main(String[] args) {
+		int x = 10;
+		System.out.println("2倍:" + (x * 2) + ",3倍:" + (x * 3) + ",4倍:" + (x * 4));
+	}
+}

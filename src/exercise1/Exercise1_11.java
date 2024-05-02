e2128343c586a619922562e25db10e74244e2349
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 10:13:49 +0900

    Exercise1_11@練習問題1-11

---
+++ src/exercise1/Exercise1_11.java
@@ -0,0 +1,10 @@
+package exercise1;
+
+public class Exercise1_11 {
+	public static void main(String[] args) {
+		int x = 100;
+		int x1 = x / 3;
+		int x2 = x % 3;
+		System.out.println("商:" + x1 + ",余り:" + x2);
+	}
+}

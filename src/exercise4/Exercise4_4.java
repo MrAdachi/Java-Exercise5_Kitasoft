1361bdc037628d1666b0b71a23eb67d8f3c00f41
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 23:16:16 +0900

    Exercise4_4@コード内容変更

--- src/exercise4/Exercise4_4.java
+++ src/exercise4/Exercise4_4.java
@@ -2,12 +2,10 @@ package exercise4;
 
 public class Exercise4_4 {
 	public static void main(String[] args) {
-		int ans = 0;
-		int j = 1;
+		int ans = 1;
 		
 		for(int i = 0; i < 7; i++) {
-			ans = j * (i + 1);
-			j = ans;
+			ans = ans * (i + 1);
 		}
 		
 		System.out.println(ans);

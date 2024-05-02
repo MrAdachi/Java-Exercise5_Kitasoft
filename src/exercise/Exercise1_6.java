ed8c799af35e5010de0145bc8c303eb6fdf5db35
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-02 09:43:51 +0900

    Exercise1_6@練習問題1-6

---
+++ src/exercise/Exercise1_6.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class Exercise1_6 {
+	public static void main(String[] args) {
+		int x = 7;
+		x *= 3;
+		System.out.println(x);
+		x /= 2;
+		System.out.println(x);
+	}
+}

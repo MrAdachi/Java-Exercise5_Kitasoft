c1e39911987c05e1f3a7e427b39645e56ebe4b70
あだっちゃん <adachi.ryota@itfllc.co.jp>
2024-05-06 23:17:02 +0900

    Exercise6_3@練習問題6-3

---
+++ src/exercise6/Exercise6_3.java
@@ -0,0 +1,37 @@
+package exercise6;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Exercise6_3 {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.print("xの値は？ ");
+		int x = Integer.parseInt(br.readLine());
+		
+		System.out.print("yの値は？ ");
+		int y = Integer.parseInt(br.readLine());
+		
+		Exercise6_3 max_2 = new Exercise6_3();
+		
+		int ans1 = max_2.max_2num(x, y);
+		
+		System.out.println("2値で大きい値は、" + ans1 + "です。");
+		
+	}
+	
+	public int max_2num(int num1, int num2) {
+		
+		if(num1 > num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+		
+	}
+
+}
